89417d1b4be0a42eaf0a8d865022fdac152136a6
Kevin Kabore <kevin.s.kabore@gmail.com>
2017-04-11 21:41:19 -0700

    Automatically authenticates user through dispatch to store

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { Router, Route, IndexRoute, browserHistory} from 'react-router';
 import reduxThunk from 'redux-thunk';
+import { AUTH_USER } from './actions/types'
 
 import App from './components/app';
 import Feature from './components/feature';
@@ -15,9 +16,16 @@ import Welcome from './components/welcome';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
+const store = createStoreWithMiddleware(reducers)
 
+const token = localStorage.getItem('token');
+// If we have a token, consider the user to be signed in
+if (token) {
+  // update application state
+  store.dispatch({ type: AUTH_USER })
+}
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={store}>
     <Router history={browserHistory}>
       <Route path="/" component={App}>
         <IndexRoute component={Welcome}/>
